b07a230079e111a1be7f4a0dfe4322c66b6fbd60
Lucas Stopinski <lucasstopinskidasilva@gmail.com>
2026-08-24 11:47:48 -0300

    style: shorten FQCN in scramble.php per Pint's fully_qualified_strict_types
    The pre-commit hook applied this fix to the working tree but didn't
    re-stage it, so the previous commit shipped the unfixed file and CI
    caught it.

--- config/scramble.php
+++ config/scramble.php
@@ -1,6 +1,8 @@
 <?php
 
 use Dedoc\Scramble\Http\Middleware\RestrictedDocsAccess;
+use Dedoc\Scramble\SecurityDocumentation\MiddlewareAuthSecurityStrategy;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
 
 return [
     /*
@@ -170,10 +172,10 @@ return [
      * ],
      */
     'security_strategy' => [
-        \Dedoc\Scramble\SecurityDocumentation\MiddlewareAuthSecurityStrategy::class,
+        MiddlewareAuthSecurityStrategy::class,
         [
             'middleware' => ['jwt'],
-            'scheme' => \Dedoc\Scramble\Support\Generator\SecurityScheme::http('bearer'),
+            'scheme' => SecurityScheme::http('bearer'),
         ],
     ],
 ];
